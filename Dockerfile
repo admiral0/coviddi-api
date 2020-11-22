9382d98916c4fa19bb448d4ee36b5ca8683689de
Radu Andries <me@admiral0.it>
2020-11-22 16:18:48 +0100

    Experimental influx + grafana

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,9 @@ FROM python:3.8
 COPY ./requirements.txt /app/requirements.txt
 WORKDIR /app
 RUN pip install -r requirements.txt
-RUN pip install uwsgi
 COPY . /app
 VOLUME ['/coviddi']
 ENV COVIDDI_HOME="/coviddi"
 EXPOSE 5000
-ENTRYPOINT [ "uwsgi"]
-CMD ["--http", "0.0.0.0:5000", "--module", "app:app"]
+ENTRYPOINT [ "/usr/local/bin/python"]
+CMD ["app.py"]
--- app.py
+++ app.py
@@ -1,128 +1,93 @@
 from typing import Optional, Callable, Any, Iterable, Mapping
 
-from flask import Flask
-from multiprocessing import Queue, Process
-from threading import Thread
-from queue import Empty
 import os
-import atexit
+import signal
+import time
+
+from influxdb import InfluxDBClient
 
 from gitcoviddi.repo import GitRepo, GitInfo
 from gitcoviddi.loader import DataLoaderItaly
 
-app = Flask(__name__, static_folder='static')
+import numpy as np
 
 COVIDDI_REPO = os.getenv('COVIDDI_REPO', 'https://github.com/pcm-dpc/COVID-19.git')
 COVIDDI_PATH = os.getenv('COVIDDI_HOME', os.path.join(os.getenv('HOME'), 'coviddimetro_data'))
+INFLUX_HOST = os.getenv('INFLUX_HOST', "")
+INFLUX_PORT = os.getenv('INFLUX_PORT', "")
+INFLUX_DB = os.getenv('INFLUX_DB', "coviddi")
+INFLUX_USER = os.getenv('INFLUXDB_USER', "")
+INFLUX_PASS = os.getenv('INFLUXDB_USER_PASSWORD', "")
+
 COVIDDI_REPO_DIR = 'Italy'
+RUN_PLS = True
 
 os.makedirs(COVIDDI_PATH, exist_ok=True)
 
-def _keep_refreshing_repo(poison_pill: Queue, results: Queue, repo_url: str, repo_path: str):
-    r = GitRepo(repo_url, repo_path)
-    l = DataLoaderItaly(repo_path)
-    results.put((r.info, l))
-    done = False
-    while not done:
-        try:
-            if r.poll():
-                try:
-                    l = DataLoaderItaly(repo_path)
-                    results.put((r.info, l))
-                except:
-                    app.logger.warning('commit %s is bad', r.info.commit_id, exc_info=True)
-            done = poison_pill.get(block=True, timeout=60*15)
-        except Empty:
-            pass
-        except:
-            app.logger.warning('could not pull from git', exc_info=True)
+def signal_handler(signal, frame):
+    global RUN_PLS
+    RUN_PLS = False
+    print("Exiting")
+
+signal.signal(signal.SIGINT, signal_handler)
 
 
-poison = Queue()
-updates = Queue()
-process = Process(
-    target=_keep_refreshing_repo,
-    args=(poison, updates, COVIDDI_REPO, os.path.join(COVIDDI_PATH, COVIDDI_REPO_DIR))
-)
-process.start()
+def main():
+    repo_path = os.path.join(COVIDDI_PATH, COVIDDI_REPO_DIR)
+    print("Reading repo....")
+    r = GitRepo(COVIDDI_REPO, repo_path)
+    r.poll()
+    current_commit = r.info.commit_id
+    print(f"repo at {current_commit}")
+    time.sleep(10)
+    update(repo_path)
 
-def _cleanup():
-    poison.put(True)
-    process.join(timeout=10)
-
-atexit.register(_cleanup)
-
-INFO: GitInfo
-DATA: DataLoaderItaly
-
-INFO, DATA = updates.get(block=True)
-
-class DataUpdater(Thread):
-
-    def __init__(self, group: None = ..., target: Optional[Callable[..., Any]] = ..., name: Optional[str] = ...,
-                 args: Iterable[Any] = ..., kwargs: Mapping[str, Any] = ..., *, daemon: Optional[bool] = ...) -> None:
-        super().__init__(group, target, name, args, kwargs, daemon=daemon)
-        self.daemon = True
-
-    def run(self) -> None:
-        global INFO
-        global DATA
-        while True:
-            INFO, DATA = updates.get(block=True)
+    while RUN_PLS:
+        r.poll()
+        if current_commit == r.info.commit_id:
+            time.sleep(10)
+            continue
+        print(f"{r.info.commit_time} - Updated repo from {current_commit} to {r.info.commit_id}")
+        current_commit = r.info.commit_id
+        update(repo_path)
 
 
-@app.route('/api/android/v1/')
-def android_api_v1():
-    r = app.response_class(
-        response=DATA.android_bundle_v1,
-        status=200,
-        mimetype='application/json'
-    )
-    return r
-
-@app.route('/api/v1/italy')
-def api_v1_italy():
-    response = app.response_class(
-        response=DATA.italy,
-        status=200,
-        mimetype='text/csv'
-    )
-    response.headers['X-Commit-Id'] = INFO.commit_id
-    response.headers['X-Last-Updated'] = INFO.commit_time.timestamp()
-    return response
-
-@app.route('/api/v1/regioni')
-def api_v1_regioni():
-    response = app.response_class(
-        response=DATA.regions,
-        status=200,
-        mimetype='text/csv'
-    )
-    response.headers['X-Commit-Id'] = INFO.commit_id
-    response.headers['X-Last-Updated'] = INFO.commit_time.timestamp()
-    return response
+def update(path):
+    l = DataLoaderItaly(path)
+    with open(os.path.join(COVIDDI_PATH, "android.v1.json"), "w") as fp:
+        print("Writing android bundle")
+        fp.write(l.android_bundle_v1)
+    if INFLUX_HOST != "":
+        print("Saving to influx")
+        save(l)
 
 
-@app.route('/api/v1/province')
-def api_v1_province():
-    response = app.response_class(
-        response=DATA.regions,
-        status=200,
-        mimetype='text/csv'
-    )
-    response.headers['X-Commit-Id'] = INFO.commit_id
-    response.headers['X-Last-Updated'] = INFO.commit_time.timestamp()
-    return response
 
 
-@app.route('/api/v1/status')
-def api_v1_status():
-    return {
-        'status': 'ok',
-        'last_update': INFO.commit_time.timestamp(),
-        'commit_id': INFO.commit_id,
-    }
+def save(l: DataLoaderItaly):
+    db = InfluxDBClient(INFLUX_HOST, int(INFLUX_PORT), INFLUX_USER, INFLUX_PASS, INFLUX_DB)
+    db.switch_database(INFLUX_DB)
+
+    pp = []
+    for date, fields in l.italy.to_dict('index').items():
+        add_fields = {}
+        for k,v in fields.items():
+            if type(v) is str:
+                continue
+            if not np.isnan(v):
+                add_fields[k]=int(v)
+        #print(add_fields)
+        pp.append({
+            "measurement": 'italia',
+            "time": int(date.value),
+            "fields": add_fields
+        })
+    
+
+    db.write_points(pp)
+    #db.write_points(l.regions, 'regioni')
+    #db.write_points(l.provinces, 'province')
 
 
 if __name__ == '__main__':
-    app.run()
+    main()
